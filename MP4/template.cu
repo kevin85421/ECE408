508f3829ad8d96553a8ffe2ba53c38dbe0867ed5
Kai-Hsun Chen <b03901153@ntu.edu.tw>
2022-09-17 22:00:56 -0700

    tmp

--- MP4/template.cu
+++ MP4/template.cu
@@ -11,12 +11,42 @@
   } while (0)
 
 //@@ Define any useful program-wide constants here
+#define MASK_WIDTH 3
 
 //@@ Define constant memory for device kernel here
+__constant__ float Mc[MASK_WIDTH][MASK_WIDTH][MASK_WIDTH];
 
 __global__ void conv3d(float *input, float *output, const int z_size,
                        const int y_size, const int x_size) {
   //@@ Insert kernel code here
+  const int TILE_WIDTH = 16;
+  int tx = threadIdx.x; int ty = threadIdx.y; int tz = threadIdx.z;
+  int xo = blockIdx.x * TILE_WIDTH + tx;
+  int yo = blockIdx.y * TILE_WIDTH + ty;
+  int zo = blockIdx.z * TILE_WIDTH + tz;
+  int xi = xo - (MASK_WIDTH / 2); int yi = yo - (MASK_WIDTH / 2); int zi = zo - (MASK_WIDTH / 2);
+  __shared__ float tile[TILE_WIDTH + MASK_WIDTH - 1][TILE_WIDTH + MASK_WIDTH - 1][TILE_WIDTH + MASK_WIDTH - 1];
+
+  float Pvalue = 0.0f;
+  if ((xi >= 0) && (xi < x_size) && (yi >= 0) && (yi < y_size) && (zi >= 0) && (zi < z_size))
+    tile[tz][ty][tx] = input[zi * x_size * y_size + yi * x_size + xi];
+  else
+    tile[tz][ty][tx] = 0.0f;
+  __syncthreads();
+
+  if ((tz < TILE_WIDTH) && (ty < TILE_WIDTH) && (tx < TILE_WIDTH)) {
+    for (int i=0; i < MASK_WIDTH; i++) {
+      for (int j=0; j < MASK_WIDTH; j++) {
+        for (int k=0; k < MASK_WIDTH; k++) {
+          Pvalue += Mc[i][j][k] * tile[i+tz][j+ty][k+tx];
+        }
+      }
+    }
+
+    if ((xo < x_size) && (yo < y_size) && (zo < z_size)) {
+      output[zo * x_size * y_size + yo * x_size + xo + 3] = Pvalue;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -53,19 +83,29 @@ int main(int argc, char *argv[]) {
   //@@ Allocate GPU memory here
   // Recall that inputLength is 3 elements longer than the input data
   // because the first  three elements were the dimensions
+  cudaMalloc((void **) &deviceInput, (inputLength - 3) * sizeof(float));
+  cudaMalloc((void **) &deviceOutput, inputLength * sizeof(float));
   wbTime_stop(GPU, "Doing GPU memory allocation");
 
   wbTime_start(Copy, "Copying data to the GPU");
   //@@ Copy input and kernel to GPU here
   // Recall that the first three elements of hostInput are dimensions and
-  // do
-  // not need to be copied to the gpu
+  // do not need to be copied to the gpu
+  // 1. Copy input (= N) to device memory
+  cudaMemcpy(deviceInput, &hostInput[3], (inputLength - 3) * sizeof(float), cudaMemcpyHostToDevice);
+  // 2. Copy kernel (= mask = M) to constant memory
+  cudaMemcpyToSymbol(Mc, hostKernel, kernelLength * sizeof(float));
   wbTime_stop(Copy, "Copying data to the GPU");
 
   wbTime_start(Compute, "Doing the computation on the GPU");
   //@@ Initialize grid and block dimensions here
+  // kernelLength => MASK_WIDTH ^ 3
+  int BLOCK_WIDTH = 16;
+  dim3 dimGrid(ceil((1.0 * x_size)/BLOCK_WIDTH), ceil((1.0 * y_size)/BLOCK_WIDTH), ceil((1.0 * z_size)/BLOCK_WIDTH));
+  dim3 dimBlock(BLOCK_WIDTH + MASK_WIDTH - 1, BLOCK_WIDTH + MASK_WIDTH - 1, BLOCK_WIDTH + MASK_WIDTH - 1);
 
   //@@ Launch the GPU kernel here
+  conv3d<<<dimGrid, dimBlock>>>(deviceInput, deviceOutput, z_size, y_size, x_size);
   cudaDeviceSynchronize();
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
@@ -73,6 +113,7 @@ int main(int argc, char *argv[]) {
   //@@ Copy the device memory back to the host here
   // Recall that the first three elements of the output are the dimensions
   // and should not be set here (they are set below)
+  cudaMemcpy(hostOutput, deviceOutput, inputLength * sizeof(float), cudaMemcpyDeviceToHost);
   wbTime_stop(Copy, "Copying data from the GPU");
 
   wbTime_stop(GPU, "Doing GPU Computation (memory + compute)");
@@ -81,6 +122,18 @@ int main(int argc, char *argv[]) {
   hostOutput[0] = z_size;
   hostOutput[1] = y_size;
   hostOutput[2] = x_size;
+
+  // for (int i=0; i < MASK_WIDTH; i++) {
+  //   for (int j=0; j < MASK_WIDTH; j++) {
+  //     for (int k=0; k < MASK_WIDTH; k++) {
+  //       wbLog(TRACE, "Mc[", i, "][", j, "][", k, "] = ", Mc[i * MASK_WIDTH * MASK_WIDTH + j * MASK_WIDTH + k]);
+  //       wbLog(TRACE, "hostKernel[", i, "][", j, "][", k, "] = ", hostKernel[i * MASK_WIDTH * MASK_WIDTH + j * MASK_WIDTH + k]);  
+  //     }
+  //   }
+  // }
+  for (int i=0; i < inputLength; i++) {
+    wbLog(TRACE, "hostOutput[", i, "] = ", hostOutput[i]);
+  }
   wbSolution(args, hostOutput, inputLength);
 
   // Free device memory
